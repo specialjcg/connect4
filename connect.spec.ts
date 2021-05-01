b33b1aca5227116ef4c10cadaa86037114fd16d8
jcgouleau <jean-charles.gouleau@savoye.com>
2021-05-01 10:57:21 +0200

    first commit

---
+++ connect.spec.ts
@@ -0,0 +1,39 @@
+export enum Pawn {
+    RED = 1,
+    YELLOW = -1,
+    EMPTY = 0
+}
+
+class Grid {
+    private line: Pawn[] = new Array(7);
+
+    full() {
+        return false;
+    }
+
+    getPawnAtPosition(column: number, line: number) {
+        return this.line[column];
+    }
+
+    addPawn(pawn: Pawn, column: number) {
+        this.line[column] = pawn;
+    }
+}
+
+describe('test connect 4', () => {
+    let grid: Grid;
+    beforeEach(() => {
+        grid = new Grid();
+    });
+    it('should have an empty grid', () => {
+        expect(grid.full()).toEqual(false);
+    });
+    it('should get  Red pawn at position column 0, line 0 ', () => {
+        grid.addPawn(Pawn.RED, 0);
+        expect(grid.getPawnAtPosition(0, 0)).toEqual(Pawn.RED);
+    });
+    it('should get Yellow at position column 0, line 0', () => {
+        grid.addPawn(Pawn.YELLOW, 0);
+        expect(grid.getPawnAtPosition(0, 0)).toEqual(Pawn.YELLOW)
+    });
+});
