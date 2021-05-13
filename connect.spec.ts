338de6ac11e9c1b749df82330c53a2cfe3d9e6b1
jcgouleau <jean-charles.gouleau@savoye.com>
2021-05-13 10:33:58 +0200

    Changes by kyappy

--- connect.spec.ts
+++ connect.spec.ts
@@ -10,20 +10,21 @@ let ROWS:number=6;
 
 class Grid {
     private pawns: Pawn[] = Array.from({length:COLUMNS * ROWS}).map(()=>Pawn.EMPTY);
-    private line: Pawn[] = Array.from({length:7}).map(()=>Pawn.EMPTY);
-  //  line1:  Pawn[] = Array.from({length:7}).map(()=>Pawn.EMPTY);
-
 
     full() {
-        return this.line.reduce((acc,pawn)=>acc && pawn!==Pawn.EMPTY,true);
+        return this.pawns.reduce((acc,pawn)=>acc && pawn!==Pawn.EMPTY,true);
     }
 
-    getPawnAtPosition(column: number, line: number) {
-        return this.line[column];
+    getPawnAtPosition(column: number): Pawn {
+        return this.pawns[column];
     }
 
-    addPawn(pawn: Pawn, column: number,row :number): void {
-        this.setPawnAtPosition(pawn, column, row);
+    setPawnAtPosition(pawn : Pawn, column: number): void {
+        this.pawns[column] = pawn;
+    }
+
+    addPawn(pawn: Pawn, column: number): void {
+        this.setPawnAtPosition(pawn, column);
     }
 }
 
@@ -35,16 +36,12 @@ describe('test connect 4', () => {
     it('should have an empty grid', () => {
         expect(grid.full()).toEqual(false);
     });
-    it('should get  Red pawn at position column 0, line 0 ', () => {
-        grid.addPawn(Pawn.RED, 0,0);
-        expect(grid.getPawnAtPosition(0, 0)).toEqual(Pawn.RED);
+    it('should get Red pawn at position column 0 ', () => {
+        grid.addPawn(Pawn.RED, 0);
+        expect(grid.getPawnAtPosition(0)).toEqual(Pawn.RED);
     });
-    it('should get Yellow at position column 0, line 0', () => {
-        grid.addPawn(Pawn.YELLOW, 0,0);
-        expect(grid.getPawnAtPosition(0, 0)).toEqual(Pawn.YELLOW)
+    it('should get Yellow at position column 0', () => {
+        grid.addPawn(Pawn.YELLOW, 0);
+        expect(grid.getPawnAtPosition(0)).toEqual(Pawn.YELLOW)
     });
-    it('should add a pawn on line 1',()=>{
-        grid.addPawn(Pawn.YELLOW, 0, 1);
-        expect(grid.line1[0]).toEqual(Pawn.YELLOW)
-    })
 });
