0f255e4cbdfd07264ffda9b1bf8fea26424770a3
jcgouleau <jean-charles.gouleau@savoye.com>
2021-06-05 10:18:40 +0200

    Changes by iteration 17

--- connect.spec.ts
+++ connect.spec.ts
@@ -8,18 +8,26 @@ const COLUMNS: number = 7;
 
 const ROWS: number = 6;
 
+const BOARD_DIMENSION: number = COLUMNS * ROWS;
+
+class Column {
+    constructor(public readonly index: number) {
+        if (index < 0 || index > 6) {
+            throw new IllegalColumnIndexError();
+        }
+    }
+}
+
+class IllegalColumnIndexError {}
+
 class Grid {
-    private pawns: Pawn[] = Array.from({length: DIMENSION}).map(() => Pawn.EMPTY);
+    private pawns: Pawn[] = Array.from({length: BOARD_DIMENSION}).map(() => Pawn.EMPTY);
 
     private static toOneDimension(column: number, row: number) {
         return column + row * COLUMNS;
     }
 
-    private static nextLine(index: number) {
-        if (index + COLUMNS >= ROWS * COLUMNS) {
-            throw new RangeError();
-        }
-
+    private static nextLineIndex(index: number): number {
         return index + COLUMNS;
     }
 
@@ -36,10 +44,14 @@ class Grid {
     }
 
     private insertPawnInPawnsCollection(pawn: Pawn, index: number): void {
+        const NEXT_LINE_INDEX: number = Grid.nextLineIndex(index);
+
         if (this.pawns[index] === Pawn.EMPTY) {
             this.pawns[index] = pawn;
+        } else if (NEXT_LINE_INDEX < BOARD_DIMENSION) {
+            this.insertPawnInPawnsCollection(pawn, NEXT_LINE_INDEX);
         } else {
-            this.insertPawnInPawnsCollection(pawn, Grid.nextLine(index));
+            throw new RangeError();
         }
     }
 }
@@ -67,9 +79,9 @@ describe('test connect 4', () => {
         expect(grid.getPawnAtPosition(0, 1)).toEqual(Pawn.YELLOW)
     });
     it('should add pawn Red,Yellow,Red: 1 at position column 1', () => {
-        grid.addPawn(Pawn.RED, 1);
-        grid.addPawn(Pawn.YELLOW, 1);
-        grid.addPawn(Pawn.RED, 1);
+        grid.addPawn(Pawn.RED, new Column(1));
+        grid.addPawn(Pawn.YELLOW, new Column(1));
+        grid.addPawn(Pawn.RED, new Column(1));
         expect(grid.getPawnAtPosition(1, 0)).toEqual(Pawn.RED)
         expect(grid.getPawnAtPosition(1, 1)).toEqual(Pawn.YELLOW)
         expect(grid.getPawnAtPosition(1, 2)).toEqual(Pawn.RED)
@@ -88,7 +100,13 @@ describe('test connect 4', () => {
         }
 
     });
-    // TODO: fills the columns and manage edge cases
+    it('should not add pawn in column 7 ', () => {
+        expect(() => grid.addPawn(Pawn.RED, new Column(7))).toThrow(IllegalColumnIndexError);
+    });
+    it('should not add pawn in column -1 ', () => {
+        expect(() => grid.addPawn(Pawn.RED, new Column(-1))).toThrow(IllegalColumnIndexError);
+    });
+    // TODO: fills the columns and manage edge cases!
     // TODO: Test victory conditions
     // TODO: Core / Generic : prints (contrat d'interface ?).
 });
